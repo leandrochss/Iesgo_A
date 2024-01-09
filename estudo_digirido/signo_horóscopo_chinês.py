703af3a1f08a19a15382438de1a9c1ae65769bcd
Leandro <142061828+leandrochss@users.noreply.github.com>
2024-01-09 19:33:28 -0300

    Delete estudo_digirido directory

--- estudo_digirido/signo_horóscopo_chinês.py
+++
@@ -1,9 +0,0 @@
-# Exercício 6: Faça um programa que solicita o ano de nascimento do usuário e retorna o seu signo no horóscopo chinês.
-
-def horoscopo_chines():
-    ano_nascimento = int(input("Digite o seu ano de nascimento: "))
-    signos = ['Macaco', 'Galo', 'Cão', 'Porco', 'Rato', 'Boi', 'Tigre', 'Coelho', 'Dragão', 'Serpente', 'Cavalo', 'Cabra']
-    signo = signos[ano_nascimento % 12]
-    print("O seu signo no horóscopo chinês é: ", signo)
-
-horoscopo_chines()
